328a90b3e6c16c54b867f188ee5ef0131f0c0637
VJS <minsung_96@naver.com>
2021-02-19 19:21:21 +0900

    Changed nav to Buttons, Modal next week

--- nav.js
+++ nav.js
@@ -1,26 +1,48 @@
-const nav = document.querySelector(".js-nav")
+const navAbout = document.querySelector("#js-navAbout")
+const navMain = document.querySelector("#js-navMain")
+const navContact = document.querySelector("#js-navContact")
 
-function handleHover(event) {
-    nav.style.color = "#394ae6"
-    nav.style.textShadow = "0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff"
+
+function handleHoverAbout(event) {
+    navAbout.style.color = "rgb(255, 252, 95)"
+    navAbout.style.textShadow = "0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff"
+
 }
 
-function handleOut(event) {
-    nav.style.color = "white"
-    nav.style.textShadow = "0"
+function handleHoverMain(event) {
+    navMain.style.color = "rgb(255, 252, 95)"
+    navMain.style.textShadow = "0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff"
+
 }
 
+function handleHoverContact(event) {
+    navContact.style.color = "rgb(255, 252, 95)"
+    navContact.style.textShadow = "0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff"
+
+}
+
+function handleOutAbout(event) {
+    navAbout.style.color = "white"
+    navAbout.style.textShadow = "none"
+}
+
+function handleOutMain(event) {
+    navMain.style.color = "white"
+    navMain.style.textShadow = "none"
+}
+
+function handleOutContact(event) {
+    navContact.style.color = "white"
+    navContact.style.textShadow = "none"
+}
 function init(){
-    window.onscroll = function hold(){
-        if(window.pageYOffset > nav[0].offSetTop) {
-            nav[0].classList.add("nav")
-        } else {
-            nav[0].classList.remove("nav")
-        }
-    }
 
-    nav.addEventListener("mouseover", handleHover)
-    nav.addEventListener("mouseout", handleOut)
+    navAbout.addEventListener("mouseover", handleHoverAbout)
+    navAbout.addEventListener("mouseout", handleOutAbout)
+    navMain.addEventListener("mouseover", handleHoverMain)
+    navMain.addEventListener("mouseout", handleOutMain)
+    navContact.addEventListener("mouseover", handleHoverContact)
+    navContact.addEventListener("mouseout", handleOutContact)
 }
 
 init()
